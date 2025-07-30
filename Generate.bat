c1292f8803e278968217b8ef36cc36fe06f15b4a
Kaihou <80059946+kaihouguide@users.noreply.github.com>
2025-07-30 13:25:12 +0300

    Update Generate.bat

--- Generate.bat
+++ Generate.bat
@@ -1,1 +1,1 @@
-python process_pdfs.py --training-folder training_data --worksheets-folder my_worksheets --max-workers 5
+python process_pdfs.py --training-folder training_data --worksheets-folder my_worksheets --max-workers 1
